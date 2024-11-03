340ae0b2d44f95757cdfa81589e57928dcbb8834
Pramuditha Jayawardhana <pramusj@Pramudithas-MacBook-Air-2.local>
2024-11-03 20:01:00 +0800

    initital setup

---
+++ models/joke-mode.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const jokeSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  type: { type: String, required: true },
+  status: {
+    type: String,
+    enum: ["pending", "approved", "rejected"],
+    default: "pending",
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Joke", jokeSchema);
